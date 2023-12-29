62c252a34f5f3e3f85a2a5067ad989d0201d67a8
Pratham Choudhary <cpratham66@gmail.com>
2023-12-30 01:40:35 +0530

    Search bar added

--- app.js
+++ app.js
@@ -21,3 +21,34 @@ const observer = new IntersectionObserver((entries) => {
 }, { rootMargin: '0px 0px -30% 0px' });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+function toggleSearchBar() {
+    var searchBar = document.getElementById("searchBar");
+    searchBar.classList.toggle("active");
+
+    if (searchBar.classList.contains("active")) {
+        document.addEventListener("click", clickOutsideSearchBar);
+        window.addEventListener("scroll", hideOnScroll);
+    } else {
+        document.removeEventListener("click", clickOutsideSearchBar);
+        window.removeEventListener("scroll", hideOnScroll);
+    }
+}
+
+function clickOutsideSearchBar(event) {
+    var searchBar = document.getElementById("searchBar");
+    var fixedImage = document.querySelector(".fixed-image");
+
+    if (!searchBar.contains(event.target) && !fixedImage.contains(event.target)) {
+        searchBar.classList.remove("active");
+        document.removeEventListener("click", clickOutsideSearchBar);
+        window.removeEventListener("scroll", hideOnScroll);
+    }
+}
+
+function hideOnScroll() {
+    var searchBar = document.getElementById("searchBar");
+    searchBar.classList.remove("active");
+    document.removeEventListener("click", clickOutsideSearchBar);
+    window.removeEventListener("scroll", hideOnScroll);
+}
